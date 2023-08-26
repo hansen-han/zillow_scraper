5cac26d8d8ceba9632c38f803df1aae797571e56
Hansen Han <61120325+hansenrhan@users.noreply.github.com>
2023-08-25 22:15:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
         'requests>=2.25.1',
         'bs4>=0.0.1',
         'pandas>=1.1.5',
-        # Note: packages in the standard library are not listed here
     ],
     author='Hansen Han',
     author_email='hansenrjhan@gmail.com',
